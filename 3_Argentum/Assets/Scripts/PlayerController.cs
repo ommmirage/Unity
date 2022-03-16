6d7fc495940b0caae72b2a9e57ef985cc54967cc
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2022-03-16 09:44:25 +0400

    Move player

--- 3_Argentum/Assets/Scripts/PlayerController.cs
+++ 3_Argentum/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float controllSpeed = 10f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 10f;
+
     void Update()
     {
         float xThrow = Input.GetAxis("Horizontal");
-        Debug.Log(xThrow);
+        float yThrow = Input.GetAxis("Vertical");
 
-        float yThrow = Input.GetAxis("vertical");
-        Debug.Log(yThrow);
+        float xOffset = xThrow * Time.deltaTime * controllSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controllSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(
+            clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
