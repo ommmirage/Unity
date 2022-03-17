26a1f4fe1f19a64aaabe043a87e9f36fa6e0c74e
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2022-03-17 11:24:14 +0400

    Explosion

--- 3_Argentum/Assets/Scripts/CollisionHandler.cs
+++ 3_Argentum/Assets/Scripts/CollisionHandler.cs
@@ -1,13 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other) {
-        Debug.Log($"Player collided with {other.gameObject.name}");
+    [SerializeField] float loadDelay = 2f;
+    [SerializeField] ParticleSystem explosionParticles;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        StartCrashSequence();
     }
+
+    private void StartCrashSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        explosionParticles.Play();
+        Invoke("ReloadLevel", loadDelay);
+    }
+
     private void OnParticleCollision(GameObject other) {
         Debug.Log($"Particles collided with {other.gameObject.name}");
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
