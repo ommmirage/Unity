563017b560e2ce113c3214a9b755fdb24fa9b5ec
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2022-03-19 08:41:50 +0400

    Score

--- 3_Argentum/Assets/Scripts/ScoreBoard.cs
+++ 3_Argentum/Assets/Scripts/ScoreBoard.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    void Start() 
+    {
+        scoreText = GetComponent<TMP_Text>();
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
+        scoreText.text = "SCORE: " + score;
     }
 }
