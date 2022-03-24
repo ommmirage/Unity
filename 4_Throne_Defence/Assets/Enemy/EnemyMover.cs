cc53b2cb2eed9779be1c9a2c229deacb91d2c9c2
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2022-03-24 22:15:59 +0400

    Enemy

---
+++ 4_Throne_Defence/Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+    void Start()
+    {
+        PrintWaypointName();
+    }
+
+    void PrintWaypointName()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+        }
+    }
+}
