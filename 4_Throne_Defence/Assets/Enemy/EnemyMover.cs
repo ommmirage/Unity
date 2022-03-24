01bb950e9890b1b875db5c12552b8b2f81563ca0
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2022-03-24 22:48:06 +0400

    Move enemy

--- 4_Throne_Defence/Assets/Enemy/EnemyMover.cs
+++ 4_Throne_Defence/Assets/Enemy/EnemyMover.cs
@@ -1,20 +1,23 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float delay = 0.5f;
 
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(delay);
         }
     }
 }
