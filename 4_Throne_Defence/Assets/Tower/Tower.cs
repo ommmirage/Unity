d641ed4eb47d2879e829d038d48a434b5c05e5f0
Sergey Miroshnichenko <ommmirage@gmail.com>
2022-04-01 09:36:44 +0400

    Build timer

--- 4_Throne_Defence/Assets/Tower/Tower.cs
+++ 4_Throne_Defence/Assets/Tower/Tower.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 10;
+    [SerializeField] float bildDelay = 0.2f;
+
+    void Start()
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower towerPrefab, Transform transform)
     {
         Bank bank = FindObjectOfType<Bank>();
+
+        if (bank == null) { return false; }
 
         if (bank.Balance >= cost)
         {
@@ -17,5 +25,25 @@ public class Tower : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        transform.GetChild(1).GetChild(0).gameObject.SetActive(false);
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(bildDelay);
+        }
+
+        yield return new WaitForSeconds(bildDelay);
+
+        transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
+
     }
 }
